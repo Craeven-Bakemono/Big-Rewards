e08a8cea55523b462bfa9c51349d4a131bffdc21
Craeven-Bakemono <127590124+Craeven-Bakemono@users.noreply.github.com>
2025-08-30 19:39:56 +0200

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,3 +1,5 @@
+import myImage from "../assets/testimonial1.jpg"
+
 export default function Testimonials() {
   return (
     <section className="bg-gray-50 py-8 px-6">
@@ -5,11 +7,7 @@ export default function Testimonials() {
         What Our Winners Say
       </h2>
       <div className="max-w-md mx-auto bg-white shadow rounded-2xl p-6 text-center">
-        <img
-          src="/src/assets/testimonial1.jpg"
-          alt="Testimonial"
-        //   className="w-24 h-24 rounded-full mx-auto mb-4"
-        />
+        <img src={myImage} alt="Testimonial" />
         <p className="italic text-lg">
           “I thought it was a scam but I actually received my $750 Shein card.
           So worth it!”
